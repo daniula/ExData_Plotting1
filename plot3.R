27341e94708040a840f411561e88762d24c72953
Daniel Mendalka <daniel.mendalka@gmail.com>
2015-03-06 06:04:02 +0100

    in plot3 move legend() inside with(read_data()) block

--- plot3.R
+++ plot3.R
@@ -12,11 +12,11 @@ with(read_data(), {
     )
     lines(DateTime, Sub_metering_2, type = "l", col = "red")
     lines(DateTime, Sub_metering_3, type = "l", col = "blue")     
+    
+    legend("topright", lwd = 1,
+           col = c("black", "red", "blue"),
+           legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+    )
 })
-
-legend("topright", lwd = 1,
-       col = c("black", "red", "blue"),
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-)
 
 dev.off()
